399ccdab48869956c7d79ca3825c888c647cfa9c
Oohira Suguru <suguru.13579515@gmail.com>
2020-12-19 15:34:29 +0900

    study react redux

--- src/App.js
+++ src/App.js
@@ -1,66 +1,117 @@
 import React,{Component} from 'react';
+import { connect } from 'react-redux';
 import './App.css';
 import Rect from './Rect';
 
 // let data = {title:'title',
 //             message:'sample'}
 
-let theme ={
-  light:{
-    backgroundColor:"#eef",
-    color:"#006",
-    padding:"10px"
-  },
-  dark:{
-    backgroundColor:"#006",
-    color:"#eef",
-    padding:"10px"
-  }
+// ステートのマッピング
+function mappongState(state){
+  return state;
 }
 
-const ThemeContext = React.createContext(theme.dark);
+// let theme ={
+//   light:{
+//     backgroundColor:"#eef",
+//     color:"#006",
+//     padding:"10px"
+//   },
+//   dark:{
+//     backgroundColor:"#006",
+//     color:"#eef",
+//     padding:"10px"
+//   }
+// }
+
+// const ThemeContext = React.createContext(theme.dark);
 
 class App extends Component{
-  static contextType = ThemeContext;
+  // static contextType = ThemeContext;
 
   // newdata = {
   //   title:'newtitle',
   //   message:'newSample'
   // }
 
+  // constructor(props){
+  //   super(props);
+  // }
+
   render(){
     return(
-      <div style={this.context}>
-        <Title value="Content age" />
-        <Message value="This is Content sample" />
-        <Message value="This is Theme sample" />
+      <div >
+        <h1>Redux</h1>
+        <Message />
+        <Button />
       </div>      
     );
   }
 }
+// ストアのコネクト
+App = connect()(App);
 
-class Title extends Component{
-  static contextType = ThemeContext;
+// class Title extends Component{
+//   static contextType = ThemeContext;
 
-  render(){
-    return (
-      <div>
-        <h2 style={this.context}>{this.props.value}</h2>
-      </div>
-    );
-  }
-}
+//   render(){
+//     return (
+//       <div>
+//         <h2 style={this.context}>{this.props.value}</h2>
+//       </div>
+//     );
+//   }
+// }
 
 class Message extends Component {
-  static contextType = ThemeContext;
+  // static contextType = ThemeContext;
+
+  style = {
+    fontSize:"20pt",
+    padding:"20px 5px"
+  }
 
   render(){
     return (
       <div>
-        <h2 style={this.context}>{this.props.value}</h2>
+        <p style={this.state}>
+          {this.props.massage}: {this.props.counter}
+        </p>
       </div>
     );
   }
 }
+// ストアのコネクト
+Message = connect(mappongState)(Message);
+
+class Button extends Component{
+  style = {
+    fontSize:"16pt",
+    padding:"5px 10px"
+  }
+
+  constructor(props){
+    super(props);
+    this.doAction = this.doAction.bind(this);
+  }
+
+  doAction(e){
+    if(e.shiftKey){
+      this.props.dispatch({type:'DECREMENT'});
+    }else{
+      this.props.dispatch({type:'INCREMENT'});
+    }
+  }
+
+  render(){
+    return(
+      <button style={this.style} onClick={this.doAction}>
+        Click
+      </button>
+    );
+  }
+}
+// ストアのコネクト
+Button = connect()(Button);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,9 +3,41 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore,combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+
+// ステートの値
+let state_value = {
+    counter:0,
+    massage:"counter"
+}
+
+// レデュサー
+function counter(state = state_value,action){
+    switch(action.type){
+        case "INCREMENT":
+        return {
+            counter:state.counter + 1,
+            massage:"INCREMENT"
+        };
+        case "DECREMENT":
+        return {
+            counter:state.counter - 1,
+            massage:"DECREMENT"
+        };
+        default:
+        return state;
+    }
+}
+
+// ストアの作成
+let store = createStore(counter);
 
 ReactDOM.render(
-    <App msg="Hello App."/>,document.getElementById('root')
+    <Provider store={store}>
+        <App msg="Hello App."/>
+    </Provider >,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
