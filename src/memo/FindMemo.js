db76af62db5a42b79067ac2d24a0c8d6f877d2f3
Oohira Suguru <suguru.13579515@gmail.com>
2020-12-20 12:02:07 +0900

    Create memo_app writed Findform.js

--- src/memo/FindMemo.js
+++ src/memo/FindMemo.js
@@ -1,12 +1,12 @@
 import React,{Component} from 'react';
 import { connect } from 'react-redux';
-import { deleteMemo } from "./Store";
+import { findMemo } from "./Store";
 
-class DelForm extends Component{
+class FindForm extends Component{
     constructor(props){
         super(props);
         this.sate = {
-            number:0
+            message:""
         }
         this.doChange = this.doChange.bind(this);
         this.doAction = this.doAction.bind(this);
@@ -20,32 +20,21 @@ class DelForm extends Component{
 
     doAction(e){
         e.prevetDefaultt();
-        let action = deleteMemo(this.state.number);
+        let action = findMemo(this.state.find);
         this.props.dispatch(action);
-        this.setState({
-            number:0
-        })
     }
 
     render(){
-        let n = 0;
-        let items = this.props.data.map((value) => {
-            <option key={n} value={n++}>
-                {value.message.substring(0,10)}
-            </option>
-        });
         return(
             <div>
                 <p>{this.props.message}</p>
                 <form onSubmit={this.doAction}>
-                    <select onChange={this.doChange} defaultValue="-1">
-                        {items}
-                    </select>
-                    <input type="submit" value="Del" />
+                    <input type="text" onChange={this.doChange} value={this.state.message}/>
+                    <input type="submit" value="Find" />
                 </form>
             </div>
         );
     }
 }
 
-export default connect ((state) => state)(DelForm)
+export default connect ((state) => state)(FindForm)
