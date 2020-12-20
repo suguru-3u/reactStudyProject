da1262aab4cfb4aa670c40a80785c98d80aeb73a
Oohira Suguru <suguru.13579515@gmail.com>
2020-12-20 11:49:24 +0900

    Create memo_app writed Addform.js

---
+++ src/memo/AddForm.js
@@ -0,0 +1,43 @@
+import React,{Component} from 'react';
+import { connect } from 'react-redux';
+import { addMemo } from "./Store";
+
+class AddForm extends Component{
+    constructor(props){
+        super(props);
+        this.sate = {
+            message:""
+        }
+        this.doChange = this.doChange.bind(this);
+        this.doAction = this.doAction.bind(this);
+    }
+
+    doChange(e){
+        this.setState({
+            message: e.target.value
+        });
+    }
+
+    doAction(e){
+        e.prevetDefaultt();
+        let action = addMemo(this.state.message);
+        this.props.dispatch(action);
+        this.setState({
+            message:""
+        })
+    }
+
+    render(){
+        return(
+            <div>
+                <p>{this.props.message}</p>
+                <form onSubmit={this.doAction}>
+                    <input type="text" onChange={this.doChange} value={this.state.message}/>
+                    <input type="submit" value="add" />
+                </form>
+            </div>
+        );
+    }
+}
+
+export default connect ((state) => state)(AddForm)
