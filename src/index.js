a5119ba40932980bf605b99e6f7c0657aeb43867
Oohira Suguru <suguru.13579515@gmail.com>
2020-12-27 08:20:01 +0900

    study firebase

--- src/index.js
+++ src/index.js
@@ -3,45 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createStore,combineReducers } from 'redux';
-import { Provider } from 'react-redux';
-import Memostore from './memo/Store';
-
-// // ステートの値
-// let state_value = {
-//     counter:0,
-//     massage:"counter"
-// }
-
-// // レデュサー
-// function counter(state = state_value,action){
-//     switch(action.type){
-//         case "INCREMENT":
-//         return {
-//             counter:state.counter + 1,
-//             massage:"INCREMENT"
-//         };
-//         case "DECREMENT":
-//         return {
-//             counter:state.counter - 1,
-//             massage:"DECREMENT"
-//         };
-//         default:
-//         return state;
-//     }
-// }
-
-// // ストアの作成
-// let store = createStore(counter);
 
 ReactDOM.render(
-    <Provider store={Memostore}>
-        <App />
-    </Provider >,
+        <App />,
     document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
