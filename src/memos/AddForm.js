41116c6454b1f9669ef6bdaad50443d1172be2d7
Oohira Suguru <suguru.13579515@gmail.com>
2021-01-06 22:24:18 +0900

    Today study React

--- src/memos/AddForm.js
+++ src/memos/AddForm.js
@@ -8,19 +8,27 @@ class AddForm extends Component{
           title:"",
           author:""
       }
+      this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange = (event) => {
+        this.setState({[event.target.name]: event.target.value})
       }
-        render() {
-          return (
+
+    render() {
+        return (
             <div className="App">
-              <p>タイトルと作者</p>
+            <p>タイトルと作者</p>
               <p>タイトル</p>
-              <input type="text" name="title"/>
+              <input type="text" name="title" onChange={this.handleChange} value={this.state.title}/>
               <p>作者</p>
-              <input type="text" name="author"/>
-            </div>
-          );
-        }
-      }
+              <input type="text" name="author" onChange={this.handleChange} value={this.state.author}/>
+              <button onClick={this.handleSubmit}>
+                send
+              </button>
+          </div>
+        );
+    }
 }
 
 export default AddForm;
