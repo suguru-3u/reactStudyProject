318e042bd7b04343bed407826295ed2dc3b8a3a9
Oohira Suguru <suguru.13579515@gmail.com>
2021-01-06 22:08:10 +0900

    Today study React

--- src/memos/AddForm.js
+++ src/memos/AddForm.js
@@ -2,57 +2,49 @@ import React,{Component} from 'react';
 
 class AddForm extends Component{
 
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
-            title : ""
-            ,body: ""
-            ,a:""
-            ,b:""
-        }
-        this.doChangeTitle = this.doChangeTitle.bind(this);
-        this.doChangeBody = this.doChangeBody.bind(this);
-        this.doAction = this.doAction.bind(this);
-    }
-
-    doChangeTitle(event){
+          isGoing: true,
+          numberOfGuests: 2
+        };
+    
+        this.handleInputChange = this.handleInputChange.bind(this);
+      }
+    
+      handleInputChange(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+    
         this.setState({
-            a: event.target.value
+          [name]: value
         });
-    }
-
-    doChangeBody(event){
-        this.setState({
-            b: event.target.value
-        });
-    }
-
-    doAction(e){
-        e.prevetDefaultt();
-        this.setState({
-            title: this.state.a
-            ,body : this.state.b
-            ,a: ""
-            ,b: ""
-        });
-    }
-
-    render(){
-        return(
-            <div>
-                <form>
-                    <input type="text" size="40"  onChange={this.doChangeTitle}/>
-                    <input type="text" size="50"   onChange={this.doChangeBody}/>
-                    <button onClick={this.doAction}>add</button>
-                </form>
-                
-                <ul>
-                    <span>{this.state.title}</span>
-                    <li>{this.state.body}</li>
-                </ul>
-            </div>
+      }
+    
+      render() {
+        return (
+          <form>
+            <label>
+              Is going:
+              <input
+                name="isGoing"
+                type="checkbox"
+                checked={this.state.isGoing}
+                onChange={this.handleInputChange} />
+            </label>
+            <br />
+            <label>
+              Number of guests:
+              <input
+                name="numberOfGuests"
+                type="number"
+                value={this.state.numberOfGuests}
+                onChange={this.handleInputChange} />
+            </label>
+          </form>
         );
-    }
+      }
 }
 
 export default AddForm;
