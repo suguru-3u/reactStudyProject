ea0a613ecdaaae7c59f8c5711a139c4e9364eab6
Oohira Suguru <suguru.13579515@gmail.com>
2021-01-06 21:37:51 +0900

    Today study React

--- src/memos/AddForm.js
+++ src/memos/AddForm.js
@@ -6,14 +6,34 @@ class AddForm extends Component{
         super(props);
         this.state = {
             value : ""
+            ,message: ""
         }
+        this.doChange = this.doChange.bind(this);
+        this.doAction = this.doAction.bind(this);
+    }
+
+    doChange(event){
+        this.setState({
+            value: event.target.value
+        });
+    }
+
+    doAction(event){
+        event.prevetDefaultt();
+        this.setState({
+            message: this.state.value
+            ,value : ""
+        });
     }
 
     render(){
         return(
             <div>
-                <input type="text" size="20" value={this.state.value} required/>
-                <button>add</button>
+                <input type="text" size="20" value={this.state.value} required onChange={this.doChange}/>
+                <button onClick={this.doAction}>add</button>
+                <ul>
+                    <li>{this.state.message}</li>
+                </ul>
             </div>
         );
     }
