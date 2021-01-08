3944e6b987253e4c961c5339d9a20455776aa99e
Oohira Suguru <suguru.13579515@gmail.com>
2021-01-08 15:04:16 +0900

    Craete MainTop

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,11 +1,12 @@
-import React,{Component} from 'react';
+import React,{Component} from "react";
+import MainTop from "./MainTop"
 
 class Main extends Component{
 
     render(){
         return(
             <div >
-              
+              <MainTop />
             </div>
         );
     }
---
+++ src/Main/MainTop.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+
+class MainTop extends Component {
+    render(){
+        return(
+            <div>
+                <h2>Topページ</h2>
+                <p>こんにちわ、こちらはBlogサイトです。Blogの投稿、編集、削除することができます。
+                </p>
+            </div>
+        );
+    }
+}
+export default MainTop
