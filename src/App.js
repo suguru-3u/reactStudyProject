eb446c31f15224d3be59c5674651bca5330780e6
Oohira Suguru <suguru.13579515@gmail.com>
2021-01-08 14:24:49 +0900

    Start Create Blog_app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React,{Component} from 'react';
 import './App.css';
 import Header from './Header/Header.js';
+import Menu from './Menu/Menu.js';
 
 class App extends Component{
 
@@ -9,6 +10,7 @@ class App extends Component{
     return(
       <div >
         <Header />
+        <Menu />
       </div>      
     );
   }
