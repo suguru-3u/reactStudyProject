d6ec412666e1fa68a4cb2c67803bad3c95345883
Oohira Suguru <suguru.13579515@gmail.com>
2021-01-09 10:45:53 +0900

    Today start study React

--- src/Main/MainBlog.js
+++ src/Main/MainBlog.js
@@ -9,12 +9,24 @@ class MainBlog extends Component {
         textAlign:"center"
     }
 
+    data = {
+        title:'blog1',
+        body:"公園に行った"
+    }
+
     render(){
         return(
             <div>
                 <h2 style={this.h2}>Blogページ</h2>
                 <p style={this.p}>blog投稿
                 </p>
+                <table>
+                    <thead>
+                        <tr>
+                            <th></th>
+                        </tr>
+                    </thead>
+                </table>
             </div>
         );
     }
