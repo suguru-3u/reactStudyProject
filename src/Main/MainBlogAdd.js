cae1a407c34bc23903b5a87db1ef4f75ed6f2ccf
Oohira Suguru <suguru.13579515@gmail.com>
2021-01-09 11:35:28 +0900

    Create MainBlogAdd.js

---
+++ src/Main/MainBlogAdd.js
@@ -0,0 +1,12 @@
+import React,{Component} from React;
+
+class MainBlogAdd extends Comment{
+    render(){
+        return(
+            <div>
+                <h2>Blog投稿</h2>
+            </div>
+        );  
+    };
+}
+export default MainBlogAdd
