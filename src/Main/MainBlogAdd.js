b5b1561bd36d5eaa1c51e602185e66ec2e2de52c
Oohira Suguru <suguru.13579515@gmail.com>
2021-01-14 06:33:19 +0900

    Start Today study React

--- src/Main/MainBlogAdd.js
+++ src/Main/MainBlogAdd.js
@@ -1,63 +1,62 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-import bloggAdd from './blogAdd'
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import React, { useState } from 'react'
 
-export default function blogindex(props) {
+export default function Blogindex(props) {
   const classes = props.style
 
-  const [values,setValues] = setValues([{
+  const [values,setValues] = useState({
     datas:[],
     title:"",
     body:""
-  }]);
+  });
 
-  
   const handleCgangeChange = event => {
-    const target = event.target;
-    const name = target.name;
-    setValues({[name]:values});
+    setValues({...values, [event.target.name]: event.target.value});
+    console.log(values.title);
+    console.log(values.body);
   }
 
   const handleSubmit = event => {
     event.preventDefault();
     console.log(values.title);
     console.log(values.body);
+    console.log(1);
     setValues({
       datas:[
-        ...datas,
+        ...values.datas,
         {
           title:values.title,
           body:values.body,
         }
       ],
-      values.title:"",
-      values.body:""
+      title:"",
+      body:""
     })
-  }
+    console.log(values.datas);
+  };
 
-
-
+  const reset = event => {
+    setValues({
+      title:"",
+      body:""
+    })
+  };
 
   return (
-    <div>
-        <form className={classes.from} noValidate autoComplete="off" onSubmit={handleSubmit}>
-            <TextField id="standard-basic" label="Title" name="title" onChange={handleCgangeChange}/>
-            <TextField
-                id="standard-basic"
-                label="Body"
-                multiline
-                rows={4}
-                name="body"
-                onChange={handleCgangeChange}
-            />
-            <Button variant="contained" onClick={() => setValues}>投稿</Button>
-            <Button variant="contained" color="secondary">リセット</Button>
-        </form>
-    </div>
+    <form className={classes.from} noValidate autoComplete="off" onSubmit={handleSubmit}>
+        <TextField id="standard-basic" label="Title" name="title" onChange={handleCgangeChange} value={values.title}/>
+        <TextField
+            id="standard-basic"
+            label="Body"
+            multiline
+            rows={4}
+            name="body"
+            onChange={handleCgangeChange}
+            value={values.body}
+        />
+        <Button variant="contained" type="submit">submit</Button>
+        <Button variant="contained" color="secondary" onClick={reset}>reset</Button>
+    </form>
   );
 }
